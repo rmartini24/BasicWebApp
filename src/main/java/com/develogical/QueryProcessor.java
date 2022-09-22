e51143666e99c943601c3dc6a4862ebe250e25c2
Rachel Martini <rmartini@andrew.cmu.edu>
2022-09-22 11:09:39 -0400

    added largest function and multiply

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -24,6 +24,37 @@ public class QueryProcessor {
             return res.toString();
         }
 
+        if (query.toLowerCase().contains("multiplied")) {
+            String[] parts = query.split(" ");
+            int first_number = Integer.parseInt(parts[2]);
+            int second_number = Integer.parseInt(parts[4]);
+            Integer res = first_number * second_number;
+            return res.toString();
+        }
+
+        if (query.toLowerCase().contains("largest")) {
+            String[] parts = query.split(" ");
+            int num1 = Integer.parseInt(parts[8].substring(0, parts[8].length()-1));
+            int num2 = Integer.parseInt(parts[9].substring(0, parts[9].length()-1));
+            int num3 = Integer.parseInt(parts[10].substring(0, parts[10].length()-1));
+            int num4 = Integer.parseInt(parts[11]);
+            Integer largest = num1;
+
+            if(num2 > largest) {
+                largest = num2;
+            }
+            if(num3 > largest) {
+                largest = num3;
+            }
+            if(num4 > largest) {
+                largest = num4;
+            }
+            return largest.toString();
+        }
+
         return "";
     }
 }
+
+//which of the following numbers is the largest: 7, 94, [43,] 918
+//20what%20is%204%20multiplied%20by%2012
